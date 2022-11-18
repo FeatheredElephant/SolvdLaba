23a682313c296ff4350cde0086e3385c410847b5
Antoni <antonikaleta@gmail.com>
2022-11-17 21:06:38 -0500

    Shop Assignment started.

--- Week1/Shop/src/Main.java
+++ Week1/Shop/src/Main.java
@@ -3,3 +3,26 @@ public class Main {
 		
 	}
 }
+
+/*
+Here are some tasks for you:
+- Create a class hierarchy (10-15 classes). Topic - Shop. For example simple app to store info about goods, employees, customers, receipts etc. I want to be able to select goods that I want to buy.
+- Create some kind of menu in console for interaction with your app: adding, editing, deleting data.
+- Add 5 interfaces to the existing hierarchy.
+- Use polymorphism with the abstract class and interface from the hierarchy.
+- Use final class, method, variable.
+- Use a static block, method, variable.
+
+Shop
+Employee
+- Manager
+Good
+- Food
+--Perishable
+- Giftcard
+Customer
+- Member
+Receipt
+Register
+Savings
+*/
