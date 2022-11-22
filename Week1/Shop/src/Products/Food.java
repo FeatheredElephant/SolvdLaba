264b12e52c76131e8ff33df54b6f6910b757e3ce
Antoni <antonikaleta@gmail.com>
2022-11-22 17:08:32 -0500

    Folder restructuring

--- Week1/Shop/src/Products/Food.java
+++
@@ -1,7 +0,0 @@
-package Products;
-public class Food extends Product {
-	// Expiration date
-	Food(String name, int cost, int stock) {
-		super(name, cost, stock);
-	}
-}
--- Week1/Shop/src/Products/Utility.java
+++
@@ -1,6 +0,0 @@
-package Products;
-public class Utility extends Product{
-	Utility(String name, int cost, int stock) {
-		super(name, cost, stock);
-	}
-}
